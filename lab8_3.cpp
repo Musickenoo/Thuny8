b381035f7af5c7d6611c12605b09e6c506e28c36
Thunyatorn Kongmoung <Thunyatorn1125@gmail.com>
2023-12-12 15:41:09 +0700

    lab8

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,51 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    string cha;
+    int bou;
+    int year,high;
+    cout << "Enter your age: ";
+    cin >> year;
+    if(year <= 25){
+        cout << "Enter your height: ";
+        cin >> high;
+        if(high < 100){ 
+            cha = "Chopper";
+            }
+        else if(high < 180){
+            cha = "Usopp";
+        }
+        else{
+            cout << "Enter your bounty: ";
+            cin >> bou;
+            if(bou <= 1100000000){
+                cha = "Sanji";
+            }
+            else{
+                cha =  "Zoro";
+            }
+        }
+        
+    }
+    else{
+        if(year <= 60){
+            cout << "Enter your bounty: ";
+            cin >> bou;
+            if(bou <= 500000000 ){
+                cha = "Franky";
+            }
+            else{
+                cha =  "Jinbe";
+            }
+        }
+        else{
+            cha = "Brook";
+        }
+    }
+    cout << "Your character = ";
+    cout << cha ;
+    
+}
+
+
